b44c4ec616fc68b84168e2694042780ccce2bea3
Vrushali Rajendra Mhaske <112822805+Vrushi95@users.noreply.github.com>
2024-06-24 14:15:49 +0530

    Add files via upload

---
+++ Addition.java
@@ -0,0 +1,11 @@
+package assignments_1;
+
+public class Addition {
+   public static void main(String[] args)
+   {
+	   int a=74;
+	   int b=36;
+	   int c=a+b;
+	   System.out.println(c);
+   }
+}
